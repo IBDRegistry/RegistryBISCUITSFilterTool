43c9a85f6983307f6fe326f7595731e15d2e3990
Alex A Dobson-Pleming <aledob@ashcombe.surrey.sch.uk>
2022-01-17 00:26:17 +0000

    Recovered from disastrous attempt to convert to .NET 5

---
+++ XML 2021K Classes/DataDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace XML_2021K_Classes.DataDictionary
+{
+    static class DataDictionary
+    {
+        static void MakeDataDictionary(DataTable DataDictionarySheet)
+        {
+
+        }
+    }
+}
