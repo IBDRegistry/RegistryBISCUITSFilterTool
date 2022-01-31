090961d69328a27700f1d68f317635f84f17c3e4
Alex A Dobson-Pleming <alexdobsonpleming@gmail.com>
2022-01-31 18:17:08 +0000

    Added date validation for consent records and behavoir for if there are no valid consent records

--- StripConsentModel/Model/Middle (Grouping)/RecordSet.cs
+++ StripConsentModel/Model/Middle (Grouping)/RecordSet.cs
@@ -5,149 +5,171 @@ using System.Text;
 
 namespace StripV3Consent.Model
 {
-    /// <summary>
-    /// A collection of all the records pertaining to a single patient
-    /// </summary>
-    public class RecordSet
-    {
-        public List<Record> Records;
+	/// <summary>
+	/// A collection of all the records pertaining to a single patient
+	/// </summary>
+	public class RecordSet
+	{
+		public List<Record> Records;
 
-        public RecordSet()
-        {
+		public RecordSet()
+		{
 
-        }
+		}
 
-        /// <summary>
-        /// Returns the value of a field that has the supplied data item code
-        /// </summary>
-        /// <param name="SpecifiedDataItemCode">Data item code of the desired field, can be found in the static DataItemCodes class</param>
-        /// <returns></returns>
-        public string GetFieldValue(string SpecifiedDataItemCode)
-        {
-            Record RecordWithValue = Records.Where(r => r.OriginalFile.SpecificationFile.Fields.Where(Field => Field.DataItemCode == SpecifiedDataItemCode).Count() > 0).First();
-            return RecordWithValue[RecordWithValue.OriginalFile.SpecificationFile.Fields.FindIndex(f => f.DataItemCode == SpecifiedDataItemCode)];
-        }
+		/// <summary>
+		/// Returns the value of a field that has the supplied data item code
+		/// </summary>
+		/// <param name="SpecifiedDataItemCode">Data item code of the desired field, can be found in the static DataItemCodes class</param>
+		/// <returns></returns>
+		public string GetFieldValue(string SpecifiedDataItemCode)
+		{
+			Record RecordWithValue = Records.Where(r => r.OriginalFile.SpecificationFile.Fields.Where(Field => Field.DataItemCode == SpecifiedDataItemCode).Count() > 0).First();
+			return RecordWithValue[RecordWithValue.OriginalFile.SpecificationFile.Fields.FindIndex(f => f.DataItemCode == SpecifiedDataItemCode)];
+		}
 
-        
+		
 
-        public ConsentValidState IsConsentValid
-        {
-            get
-            {
-                const string ConsentFileNameInSpec = "consent";
+		public ConsentValidState IsConsentValid
+		{
+			get
+			{
+				const string ConsentFileNameInSpec = "consent";
 
-                IEnumerable<Record> ConsentRecords = Records.Where(r => r.OriginalFile.SpecificationFile.Name.Contains(ConsentFileNameInSpec));
-                Record ConsentRecord;
+				IEnumerable<Record> AllConsentRecords = Records.Where(r => r.OriginalFile.SpecificationFile.Name.Contains(ConsentFileNameInSpec));
 
-                if (ConsentRecords.Count() > 1)
-                {
-                    ConsentRecord = ConsentRecords.OrderBy(r => {
-                        string RawValue = r.GetValueByDataItemCode(DataItemCodes.DateOfConsent);
-                        DateTime CastValue;
-                        if (DateTime.TryParse(RawValue, out CastValue))
+				IEnumerable<Record> ValidConsentRecords = AllConsentRecords.Where(r =>
+				{
+					string RawValue = r.GetValueByDataItemCode(DataItemCodes.DateOfConsent);
+					DateTime CastValue;
+					if (DateTime.TryParse(RawValue, out CastValue))
+					{
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				});
+
+
+				Record ConsentRecord = null;
+
+				if (ValidConsentRecords.Count() == 1)
+				{
+					ConsentRecord = ValidConsentRecords.First();
+				}
+				else if (ValidConsentRecords.Count() > 1)
+				{
+					ConsentRecord = ValidConsentRecords.OrderBy(r => {
+										string RawValue = r.GetValueByDataItemCode(DataItemCodes.DateOfConsent);
+										return DateTime.Parse(RawValue);
+					}).First();
+
+				}
+
+
+				// S251
+				if (AllConsentRecords.Count() == 0)
+				{
+					//National Opt-Out
+					IEnumerable<Record> NationalOptOutRecords = Records.Where(r => r.OriginalFile.SpecificationFile is Specification.NationalOptOutFile);
+					if (NationalOptOutRecords.Count() != 0)
+					{
+						if (NationalOptOutRecords.Any(NOORecord => NOORecord.GetValueByDataItemCode(DataItemCodes.NationalOptOut) == "Yes"))
 						{
-                            return CastValue;
-						} else
-						{
-                            throw new Exception("Failed DateTime cast for Date of Consent");
-                        }
-                    }).First();
-                }
+							return new ConsentValidState()
+							{
+								IsValid = false,
+								IsValidReason = "Patient participated in the national opt-out"
+							};
+						}
+					}
+					return new ConsentValidState()
+					{
+						IsValid = true,
+						IsValidReason = "No consent record so patient flows via S251"
+					};
+				}
 
-                // S251
-                if (ConsentRecords.Count() == 0)
-                {
-                    //National Opt-Out
-                    IEnumerable<Record> NationalOptOutRecords = Records.Where(r => r.OriginalFile.SpecificationFile is Specification.NationalOptOutFile);
-                    if (NationalOptOutRecords.Count() != 0)
-                    {
-                        if (NationalOptOutRecords.Any(NOORecord => NOORecord.GetValueByDataItemCode(DataItemCodes.NationalOptOut) == "Yes"))
-                        {
-                            return new ConsentValidState()
-                            {
-                                IsValid = false,
-                                IsValidReason = "Patient participated in the national opt-out"
-                            };
-                        }
-                    }
-                    return new ConsentValidState()
-                    {
-                        IsValid = true,
-                        IsValidReason = "No consent record so patient flows via S251"
-                    };
-                }
+				if (AllConsentRecords.Count() > 0 & ValidConsentRecords.Count() == 0 )
+				{
+					return new ConsentValidState()
+					{
+						IsValid = false,
+						IsValidReason = "No valid consent records"
+					};
+				}
 
-                ConsentRecord = ConsentRecords.First();
+				//Is Consent V4 and Yes
+				string ConsentVersion = ConsentRecord.GetValueByDataItemCode(DataItemCodes.ConsentVersionFieldID);
+				string RegistryConsent = ConsentRecord.GetValueByDataItemCode(DataItemCodes.ConsentForRegistryFieldID);
 
-                //Is Consent V4 and Yes
-                string ConsentVersion = ConsentRecord.GetValueByDataItemCode(DataItemCodes.ConsentVersionFieldID);
-                string RegistryConsent = ConsentRecord.GetValueByDataItemCode(DataItemCodes.ConsentForRegistryFieldID);
+				if (ConsentVersion == "V4" & RegistryConsent == "Y") //Check for Date of Consent as well
+				{
+					return new ConsentValidState()
+					{
+						IsValid = true,
+						IsValidReason = "V4 consent version and consented patient"
+					};
+				}
 
-                if (ConsentVersion == "V4" & RegistryConsent == "Y") //Check for Date of Consent as well
-                {
-                    return new ConsentValidState()
-                    {
-                        IsValid = true,
-                        IsValidReason = "V4 consent version and consented patient"
-                    };
-                }
-
-                if (RegistryConsent != "Y")
-                {
-                    return new ConsentValidState()
-                    {
-                        IsValid = false,
-                        IsValidReason = "Registry consent was not \"Yes\""
-                    };
-                }
+				if (RegistryConsent != "Y")
+				{
+					return new ConsentValidState()
+					{
+						IsValid = false,
+						IsValidReason = "Registry consent was not \"Yes\""
+					};
+				}
 
 #warning add check for ConsentVersion == "Pre-V4" as well as blank
-                if (ConsentVersion != "V4" & RegistryConsent == "Y")
-                {
-                    return new ConsentValidState()
-                    {
-                        IsValid = false,
-                        IsValidReason = "Consented patients are no longer valid unless they are Consent V4+"
-                    };
-                }
+				if (ConsentVersion != "V4" & RegistryConsent == "Y")
+				{
+					return new ConsentValidState()
+					{
+						IsValid = false,
+						IsValidReason = "Consented patients are no longer valid unless they are Consent V4+"
+					};
+				}
 
-                //Date of Consent
-                //User cannot save page unless version of consent, registry consent and date of consent are provided
-
-
-                return new ConsentValidState()
-                {
-                    IsValid = false,
-                    IsValidReason = "Patient did not pass consent checks"
-                };
-
-                //If Consent record is missing then true
-                //If there is an entry and consent=Yes and consent is v4 then true
-                //All other cases false
-                //If consentv4 but no consent then false
-                //If consentv3 but no consent then false
-
-            }
-        }
-
-        public override string ToString()
-        {
-            return GetFieldValue(DataItemCodes.NHSNumber);
-        }
+				//Date of Consent
+				//User cannot save page unless version of consent, registry consent and date of consent are provided
 
 
-    }
+				return new ConsentValidState()
+				{
+					IsValid = false,
+					IsValidReason = "Patient did not pass consent checks"
+				};
 
-    public class ConsentValidState
-    {
-        public bool IsValid;
+				//If Consent record is missing then true
+				//If there is an entry and consent=Yes and consent is v4 then true
+				//All other cases false
+				//If consentv4 but no consent then false
+				//If consentv3 but no consent then false
 
-        /// <summary>
-        /// Populated with the reason for passing/failing
-        /// </summary>
-        public string IsValidReason;
+			}
+		}
+
+		public override string ToString()
+		{
+			return GetFieldValue(DataItemCodes.NHSNumber);
+		}
 
 
-        public static implicit operator bool(ConsentValidState c) => c.IsValid;
-    }
+	}
+
+	public class ConsentValidState
+	{
+		public bool IsValid;
+
+		/// <summary>
+		/// Populated with the reason for passing/failing
+		/// </summary>
+		public string IsValidReason;
+
+
+		public static implicit operator bool(ConsentValidState c) => c.IsValid;
+	}
 }
