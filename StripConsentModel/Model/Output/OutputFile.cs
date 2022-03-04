7494ad55695177db0a6d18d087310d63d509ccef
Alex Dobson-Pleming <alexdobsonpleming@gmail.com>
2022-03-04 11:23:25 +0000

    Added support for puling headers from 2021K to OutputFile.RepackIntoString

--- StripConsentModel/Model/Output/OutputFile.cs
+++ StripConsentModel/Model/Output/OutputFile.cs
@@ -17,13 +17,33 @@ namespace StripV3Consent.Model
 
         public OutputFile(DataFile file) : base(file.Name) { }
         
+        private string RemoveConflictingChars(string StringToClean)
+		{
+            string[] ValuesToRemove = { "\n", "\r", "," };
+
+            foreach (string value in ValuesToRemove)
+			{
+                StringToClean = StringToClean.Replace(value, "");
+			}
+
+            return StringToClean;
+		}
 
         public string RepackIntoString()
         {
-            string[][] Content = OutputRecords.Select(r => r.DataRecord).ToArray();
+            List<string[]> Content = OutputRecords.Select(r => r.DataRecord).ToList<string[]>();
 
-            return String.Join(RowSeparator, Content.Select(
-                                record => String.Join(ColumnSeparator, record)));
+            string[] Headers = OutputRecords.First().OriginalFile.SpecificationFile.Fields.Select(field => field.Name).ToArray();
+            Headers[0] = "HEADER_" + Headers[0];    //First header must begin with HEADER_
+
+
+            string[][] OutputFileRows = new string[Content.Count() + 1][];  //the 1 is for the header row
+
+            OutputFileRows[0] = Headers.Select(header => RemoveConflictingChars(header)).ToArray();
+            Content.CopyTo(OutputFileRows, 1);
+
+            return string.Join(RowSeparator, OutputFileRows.Select(
+                                record => string.Join(ColumnSeparator, record)));
         }
 
         public static explicit operator string(OutputFile file) => file.RepackIntoString();
