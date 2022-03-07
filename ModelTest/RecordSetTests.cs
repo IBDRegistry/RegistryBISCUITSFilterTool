29d23748e65d51bb65dfdb4875ea7bba4456d33a
alexdobson <alexdobsonpleming@gmail.com>
2022-03-07 18:25:25 +0000

    Implemented tie-break scenario test for RecordSet consent check

--- ModelTest/RecordSetTests.cs
+++ ModelTest/RecordSetTests.cs
@@ -29,5 +29,28 @@ namespace ModelTest
 
 			Assert.IsTrue(TestingRecord.IsConsentValid);
 		}
+
+		/// <summary>
+		/// In the event of two records having matching DateTime values the one with Consent = N should be accepted, if it exists
+		/// </summary>
+		[TestMethod]
+		public void TestMatchingDateTimesFallback()
+		{
+			const string NHSNumber = "426 221 1894";
+			const string DOB = "14/07/1926";
+			string PatientLine = $"{NHSNumber}, {DOB}";
+			string OldConsentLine = $"{NHSNumber}, {DOB},10/05/2003,N,,,,,,,V4";
+			string NewConsentLine = $"{NHSNumber}, {DOB},10/05/2003,Y,,,,,,,V4";
+
+			ImportFile PatientFile = new("patient_Trust.csv", PatientLine);
+			ImportFile OldConsentFile = new("consent_Trust.csv", OldConsentLine);
+			ImportFile NewConsentFile = new("consent_Registry.csv", NewConsentLine);
+
+			RecordSet[] Patients = ConsentToolModel.SplitInputFilesIntoRecordSets(new ImportFile[] { PatientFile, OldConsentFile, NewConsentFile }).ToArray();
+
+			RecordSet TestingRecord = Patients.Where(rs => rs.GetFieldValue(DataItemCodes.NHSNumber) == NHSNumber).First();
+
+			Assert.IsFalse(TestingRecord.IsConsentValid);
+		}
 	}
 }
