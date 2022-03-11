bb907f8320d4e3ea3b459e92804ab09bfd6bb42d
alexdobson <alexdobsonpleming@gmail.com>
2022-03-11 16:41:41 +0000

    Fixed BottomPanel not autosizing when a large number of files were excluded in OutputFilesPanel

--- ConsentWinforms/View/Output Pane/OutputFilesPanel.cs
+++ ConsentWinforms/View/Output Pane/OutputFilesPanel.cs
@@ -18,6 +18,8 @@ namespace StripV3Consent.View
         public OutputFilesPanel()
         {
             this.Controls.Add(FileList);
+
+            FileList.BottomPanel.AutoSize = true;
         }
 
 
