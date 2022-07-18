7876c98f097c081eb0b0678d407226e1a06a4269
Alex A Dobson-Pleming <alexdobsonpleming@gmail.com>
2022-07-18 10:29:34 +0100

    Removed copy icon after scale issues

--- ConsentWinforms/Properties/Resources.Designer.cs
+++ ConsentWinforms/Properties/Resources.Designer.cs
@@ -61,16 +61,6 @@ namespace ConsentWinforms.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Copy {
-            get {
-                object obj = ResourceManager.GetObject("Copy", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon error {
